dace69d1658417e64905140524741933ad0980c4
madfalouh <70645288+madfalouh@users.noreply.github.com>
2022-05-04 01:36:56 +0200

    Update render.js

--- video-capture/src/render.js
+++ video-capture/src/render.js
@@ -4,25 +4,25 @@ const { writeFile } = require('fs');
 
 const { dialog, Menu } = remote;
 
-let mediaRecorder; // MediaRecorder instance to capture footage
+let mediaRecorder;
 const recordedChunks = [];
 // Buttons
 const videoElement = document.querySelector('video');
 
- 
+
 const startBtn = document.getElementById('startBtn');
 startBtn.onclick = e => {
-  mediaRecorder.start();
-  startBtn.classList.add('is-danger');
-  startBtn.innerText = 'Recording';
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
 };
 
 const stopBtn = document.getElementById('stopBtn');
 
 stopBtn.onclick = e => {
-  mediaRecorder.stop();
-  startBtn.classList.remove('is-danger');
-  startBtn.innerText = 'Start';
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
 };
 
 
@@ -31,7 +31,7 @@ stopBtn.onclick = e => {
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources;
 
-// Get the available video sources
+
 async function getVideoSources() {
     const inputSources = await desktopCapturer.getSources({
         types: ['window', 'screen']
@@ -50,7 +50,7 @@ async function getVideoSources() {
     videoOptionsMenu.popup();
 }
 
-// Change the videoSource window to record
+
 async function selectSource(source) {
 
     videoSelectBtn.innerText = source.name;
@@ -69,11 +69,9 @@ async function selectSource(source) {
     const stream = await navigator.mediaDevices
         .getUserMedia(constraints);
 
-    // Preview the source in a video element
     videoElement.srcObject = stream;
     videoElement.play();
 
-    // Create the Media Recorder
     const options = { mimeType: 'video/webm ; codecs=vp9' };
     mediaRecorder = new MediaRecorder(stream, options)
 
@@ -107,9 +105,8 @@ async function handleStop(e) {
     });
 
 
-   if (filePath) {
-    writeFile(filePath, buffer, () => alert("video saved"));
-  }
+    if (filePath) {
+        writeFile(filePath, buffer, () => alert("video saved"));
+    }
 }
 
- 
